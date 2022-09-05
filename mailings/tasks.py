4b41c8fe1fdecab1d5e794f6d78fc6bcf1cc2b8d
kam1dere <kam1dere@yandex.ru>
2022-09-05 16:55:20 +0300

    draft_finish_task

--- mailings/tasks.py
+++ mailings/tasks.py
@@ -1,25 +1,24 @@
 from celery import shared_task
-from .models import Mailing, Client, Message
+from django.db.models import Q
+
+from .models import Mailing, Client
 from datetime import datetime
 
 
-@shared_task
-def hi():
-    return 'Hello'
-
-@shared_task
-def mes_client():
-    return Client.objects.get(id=1)
-
-'''
-TaskResult.objects.count()
-
-'''
 @shared_task
 def mailing():
     mailing_pk = Mailing.objects.get(pk=1)
     now = datetime.now().strftime('%H:%M:%S')
 
     if mailing_pk.time_start_mailing.strftime('%H:%M:%S') <= now <= mailing_pk.time_end_mailing.strftime('%H:%M:%S'):
-        return mailing_pk
+        selection = Client.objects.filter(Q(tag__exact=mailing_pk.tag) &
+                                          Q(mobile_operator_code__exact=mailing_pk.mobile_operator_code)).values_list(
+                                            'phone_number', flat=True)
+        for client in selection:
+            print(message_for_client(mailing_pk.message_text, client))
+        return 'success'
 
+
+@shared_task
+def message_for_client(message_text, phone_number):
+    return f'Сообщение: {message_text} для {phone_number}'
